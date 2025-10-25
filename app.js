56016f1a44c2cfbfaeba755270800a90f48f0b8d
Dherenn <dheren90@gmail.com>
2025-10-25 20:38:48 +0700

    DONEEEEE

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express"
 import api from "./routes/api.js"
 import database from "./config/database.js"
 
+import passport from "passport"
+import "./config/passport.js"
+
 const app = express();
 
 app.use (express.json())
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 import express from "express"
 import * as bahanController from "../controllers/bahanController.js"
+import * as authController from "../controllers/authController.js"
+import {protect} from "../middleware/authmiddleware.js"
 
 const api = express.Router()
 
@@ -8,6 +10,9 @@ api.post("/bahan", bahanController.createBahan)
 api.put("/bahan/:id", bahanController.updateBahan)
 api.delete("/bahan/:id", bahanController.deleteBahan)
 
+api.post('/register', authController.register);
+api.post('/login', authController.login);
 
+api.get('/me',protect , authController.privateProfile);
 
 export default api
